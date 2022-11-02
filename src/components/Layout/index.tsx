e422aa3af4eebfbfe2677bc944bf2df2a5b6751f
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2022-11-02 23:21:31 +0000

    fix Back to Top button styling

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -27,8 +27,8 @@ const Layout = ({ children }: LayoutProps) => {
             })
           }
           className={clsx(
-            'text-custom-gray bg-white fixed bottom-4 right-4 grid place-items-center w-10 h-10 rounded-full md:hidden hover:bg-neutral-200 hover:translate-y-[-5px] transition-all duration-300 ease-out',
-            windowScroll < 150 && 'bottom-[-3rem]'
+            'text-custom-gray bg-white fixed right-4 grid place-items-center w-10 h-10 rounded-full md:hidden hover:bg-neutral-200 hover:translate-y-[-5px] transition-all duration-300 ease-out',
+            windowScroll < 150 ? 'bottom-[-3rem]' : 'bottom-4'
           )}
           aria-label='Back to top'
         >
